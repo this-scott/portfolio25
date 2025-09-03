7a31935afa65d1083ff5b6a009bc0026b5d85e25
this-scott <this_scott@outlook.com>
2025-09-03 17:50:56 -0400

    landing + aboutme buildout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,36 @@
 import Image from "next/image";
+import fire from "@/assets/th2025_crop.jpg"
+import aboutme_photo from "@/assets/GradPhotoCrop.jpg"
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="font-sans flex-col justify-between items-center min-h-screen pt-8 px-12 pb-20 gap-16 sm:pt-25 ">
+      <main className="flex object-fill flex-col gap-20 sm:items-start sm:gap-32">
+        <div id="first sight" className="flex flex-col">
+          <div className="flex flex-row gap-3 justify-end pr-4 pb-4">
+            <h1>blog</h1>
+            <h1>github</h1>
+            <h1>linked</h1>
+          </div>
+          <div className="flex flex-row flex-wrap justify-evenly h-full">
+            <div className="flex-1 flex flex-col items-center justify-center">
+              <div id="not another div">
+                <h4 className="text-xl">Hi I'm</h4>
+                <h1 className="font-bold font-black text-8xl">Scott Styslinger</h1>
+                <h4 className="pt-1 text-xl">This is a small about-me website. Sit back, relax, and start scrolling</h4>
+              </div>
+            </div>
+            <Image className="w-auto flex-1 h-auto min-w-100" src={fire} alt="landing_photo"></Image>
+          </div>
+        </div>
+        <div id="about me section" className="flex flex-col w-full">
+          <h1 className="text-center font-bold font-black text-6xl">About Me</h1>
+          <div className="flex flex-row flex-wrap justify-evenly h-full">
+            <div id="aboutmetext" className="flex-1 flex flex-col items-center justify-center">
+              <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam atque error nemo, tenetur minima sapiente accusantium tempora aliquid numquam nobis aut. Repellat labore repudiandae et nemo soluta! Repudiandae, corporis consectetur.</h1>
+            </div>
+            <Image className="w-1/4 h-auto object-contain" src={aboutme_photo} alt="aboutme_photo"></Image>
+          </div>
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
