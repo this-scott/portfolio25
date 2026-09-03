3053bf6814cf5d5de1d717737e08305527c1fb84
this-scott <this_scott@outlook.com>
2026-09-02 23:31:10 -0400

    Starting to log project document

---
+++ src/lib/constants.ts
@@ -0,0 +1,30 @@
+
+enum Category {
+	Robotics/Vision,
+	AI,
+	Full Stack,
+	Applications
+}
+
+enum Language {
+	Java,
+	Go,
+	Python,
+	C++,
+	C#,
+	Typescript,
+	Rust,
+	Bash
+}
+
+enum Tool {
+	ROS,
+	AWS,
+	Terraform,
+	Docker
+}
+
+enum MediaType {
+	Image,
+	IFrame
+}
